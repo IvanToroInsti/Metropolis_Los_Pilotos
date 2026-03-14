4b7a293ba61c9545393e9143ae08fa0f28e2bd1b
DiegoFdz15 <diegofernandezf06@gmail.com>
2026-03-14 15:05:54 +0100

    otro pequeño cambio

--- src/pages/CircuitPage.tsx
+++ src/pages/CircuitPage.tsx
@@ -8,6 +8,18 @@ import { puertas, tribunas, baños, bares } from "../data/points";
 
 import * as L from "leaflet";
 
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 const circuitCoords: [number, number] = [41.56919, 2.258137];
 
 // Marcador azul para WC
